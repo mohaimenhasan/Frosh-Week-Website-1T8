7503541df79f6f2752c5d701f2a8e993ad273889
John Liu <johnyuhanliu@gmail.com>
2013-04-21 11:05:40 -0400

    Set ember.js variant.

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,7 @@ module SkuleOrientation
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.ember.variant = :production
   end
 end
