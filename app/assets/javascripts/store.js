b815b5f5264e2aed608116adef9fefb4d85c96fb
John Liu <johnyuhanliu@gmail.com>
2013-06-20 02:21:25 -0700

    Removes temporary client side FIXTURE objects.

--- app/assets/javascripts/store.js
+++ app/assets/javascripts/store.js
@@ -1,9 +1,5 @@
 App.Store = DS.Store.extend({
-  adapter: 'DS.RESTAdapter',
-  init: function() {
-    this._super();
-    this.loadMany(App.Package, App.Package.FIXTURES);
-  }
+  adapter: 'DS.RESTAdapter'
 });
 
 DS.RESTAdapter.reopen({
