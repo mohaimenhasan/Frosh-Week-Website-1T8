4b2c8b9e87ca8a42ab534c5692158bcfaab70c14
Amandeep Grewal <me@amandeep.ca>
2013-06-30 02:45:01 -0700

    no db on precompile assets. silly heroku

--- config/application.rb
+++ config/application.rb
@@ -66,6 +66,8 @@ module SkuleOrientation
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
+    config.assets.initialize_on_precompile = false
+
     config.stripe_publishable_key = ENV['STRIPE_PUBLISHABLE_KEY']
     config.mandrill_from = ENV['MANDRILL_FROM']
 
