3cd0f320912321618faeaacc445a16e13fb45bb8
John Liu <johnyuhanliu@gmail.com>
2013-07-13 03:55:21 -0700

    Adds simple ui testing using sikuli.

---
+++ test/sikuli/registration-test.sikuli/registration-test.py
@@ -0,0 +1,46 @@
+bursary = True
+
+# Settings
+Settings.MoveMouseDelay = 0
+Settings.SlowMotionDelay = 0
+
+
+switchApp('Google Chrome')
+
+# Personal Information
+type("1373701450896.png", 'John\t')
+type('Liu\t')
+type('johnliu.dev@gmail.com\t')
+type('\t') # Gender
+type('\t') # Phone number
+type('\t') # Discipline
+type('\t') # Residence
+
+# Kit Information
+type('\t') # T-shirt size
+
+# Health and Safety Information
+type('Mama\t')
+type('johnliu.dev+mama@gmail.com\t')
+type('Mother\t')
+type('1231231234\t')
+type('\t') # Dietary
+type('\t') # Accessibility
+type('\t') # Misc
+
+# Payment
+if (not bursary):
+    type('\t') # Bursary
+    type('John Liu\t')
+    type('4242424242424242\t')
+    type('123\t')
+    type('12\t')
+    type('13\t')
+else:
+    type(' \t') # Bursary
+    type('10000\t')
+    type('What does this even mean?\t')
+    type("I don't need financial support.\t")
+    type('I wanna make a lot of money!!!\t')
+
+type('\n')
