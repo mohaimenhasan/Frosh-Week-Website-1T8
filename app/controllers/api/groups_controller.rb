dcdd44a2dc2a0b99b2c27a851ded92d07379d6c3
Amandeep Grewal <me@amandeep.ca>
2013-07-14 04:29:00 -0700

    Fixes error in groups controller

--- app/controllers/api/groups_controller.rb
+++ app/controllers/api/groups_controller.rb
@@ -3,11 +3,11 @@ require 'awesome_print' if Rails.env.development?
 class Api::GroupsController < ActionController::Base
 
   def show
-    render json: { user: Group.find(params[:id]) }
+    render json: { group: Group.find(params[:id]) }
   end
 
   def index
-    render json: { users: Group.all }
+    render json: { groups: Group.all }
   end
 
 end
