41aefba32085a05f88bb8e94ba92ed9ea053186d
John Liu <johnyuhanliu@gmail.com>
2013-07-22 01:42:57 -0700

    Temporarily disables about and groups link until content available.

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -8,8 +8,8 @@ App.RouteList = [
   Ember.Object.create({ route: 'index',    path: '/',          name: 'Home' }),
   Ember.Object.create({ route: 'schedule', path: '/schedule',  name: 'Events & Schedule' }),
   Ember.Object.create({ route: 'faq',      path: '/faq',       name: 'FAQs' }),
-  Ember.Object.create({ route: 'groups',   path: '/groups',    name: 'Groups' }),
-  Ember.Object.create({ route: 'about',    path: '/about',     name: 'About' }),
+//  Ember.Object.create({ route: 'groups',   path: '/groups',    name: 'Groups' }),
+//  Ember.Object.create({ route: 'about',    path: '/about',     name: 'About' }),
   Ember.Object.create({
     route: 'blog',
     path: 'http://blog.orientation.skule.ca',
