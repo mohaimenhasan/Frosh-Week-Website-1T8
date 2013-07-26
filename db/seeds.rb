b79b918eebb7162bb21e0f1bf08074484d826039
Amandeep Grewal <me@amandeep.ca>
2013-07-25 17:31:16 -0700

    Move seed data to correct place. Fix tests to always have clean db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,71 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Group.create(symbol: '&alpha;', name: 'Alpha')
+Group.create(symbol: '&beta;', name: 'Beta')
+Group.create(symbol: '&gamma;', name: 'Gamma')
+Group.create(symbol: '&Delta;', name: 'Delta')
+Group.create(symbol: '&theta;', name: 'Theta')
+Group.create(symbol: '&kappa;', name: 'Kappa')
+Group.create(symbol: '&lambda;', name: 'Lambda')
+Group.create(symbol: '&mu;', name: 'Mu')
+Group.create(symbol: '&Nu;', name: 'Ni')
+Group.create(symbol: '&Omicron;', name: 'Omicron')
+Group.create(symbol: '&pi;', name: 'Pi')
+Group.create(symbol: '&rho;', name: 'Rho')
+Group.create(symbol: '&Sigma;', name: 'Sigma')
+Group.create(symbol: '&tau;', name: 'Tau')
+Group.create(symbol: '&phi;', name: 'Phi')
+Group.create(symbol: '&Psi;', name: 'Psi')
+Group.create(symbol: '&Omega;', name: 'Omega')
+
+Package.create(
+  key: 'early-bird-standalone',
+  name: 'F!rosh Kit Early Bird Standalone Package',
+  description: 'A standard F!rosh Kit. Includes entrance to all events (like a parade!), a
+                collection of cool things (like a hard hat!), and lots more! This kit has tons
+                of awesome stuff!',
+  price: 100,
+  count: 0,
+  max: 1,
+  start_date: Date.strptime('05-01-2013', '%m-%d-%Y'),
+  end_date: Date.strptime('08-15-2013', '%m-%d-%Y'),
+)
+Package.create(
+  key: 'early-bird-with-farm',
+  name: 'F!rosh Kit Early Bird + Hart House Farm Package',
+  description: 'A standard F!rosh Kit just like above... except even better! This package
+                includes a ticket to a camping trip at Hart House Farm, located outside of
+                Caledon on the scenic Niagara Escarpment. Come join us for some fun camping
+                on September 7 and 8 to cap off F!rosh Week!',
+  price: 130,
+  count: 0,
+  max: 1,
+  start_date: Date.strptime('05-01-2013', '%m-%d-%Y'),
+  end_date: Date.strptime('08-15-2013', '%m-%d-%Y'),
+)
+Package.create(
+  key: 'early-bird-with-commuter',
+  name: 'F!rosh Kit Early Bird + Commuter Program Package',
+  description: "A standard F!rosh Kit... with living accommodations! This offer includes room
+                and board at a downtown hostel for four nights so you can fully enjoy
+                F!rosh Week's days and nights.",
+  price: 185,
+  count: 0,
+  max: 1,
+  start_date: Date.strptime('05-01-2013', '%m-%d-%Y'),
+  end_date: Date.strptime('08-15-2013', '%m-%d-%Y'),
+)
+Package.create(
+  key: 'early-bird-all',
+  name: 'F!rosh Kit Early Bird + Hart House Farm + Commuter Program Package',
+  description: 'This is for the F!rosh that wants to have the ultimate F!rosh Week experience.
+                You get a standard F!rosh kit, accommodations throughout the week, and a
+                ticket to the weekend Hart House Farm camping trip. Super fun!',
+  price: 215,
+  count: 0,
+  max: 1,
+  start_date: Date.strptime('05-01-2013', '%m-%d-%Y'),
+  end_date: Date.strptime('08-15-2013', '%m-%d-%Y'),
+)
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,5 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV["RAILS_ENV"] ||= 'test'
+ENV["RAILS_ENV"] = 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
@@ -38,3 +38,4 @@ RSpec.configure do |config|
 end
 
 Rails.logger.level = 1
+load "#{Rails.root}/db/seeds.rb"
