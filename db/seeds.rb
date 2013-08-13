4581e54a1d3942f8cc8bb77d235ed9eefb06ff42
John Liu <johnyuhanliu@gmail.com>
2013-08-13 02:12:44 -0700

    Updates early bird deadline to August 21.

--- db/seeds.rb
+++ db/seeds.rb
@@ -43,7 +43,7 @@ Package.create(
   count: 0,
   max: 1,
   start_date: Date.strptime('05-01-2013', '%m-%d-%Y'),
-  end_date: Date.strptime('08-15-2013', '%m-%d-%Y'),
+  end_date: Date.strptime('08-21-2013', '%m-%d-%Y'),
 )
 Package.create(
   key: 'early-bird-with-farm',
@@ -53,7 +53,7 @@ Package.create(
   count: 0,
   max: 1,
   start_date: Date.strptime('05-01-2013', '%m-%d-%Y'),
-  end_date: Date.strptime('08-15-2013', '%m-%d-%Y'),
+  end_date: Date.strptime('08-21-2013', '%m-%d-%Y'),
 )
 Package.create(
   key: 'early-bird-with-commuter',
@@ -63,7 +63,7 @@ Package.create(
   count: 0,
   max: 1,
   start_date: Date.strptime('05-01-2013', '%m-%d-%Y'),
-  end_date: Date.strptime('08-15-2013', '%m-%d-%Y'),
+  end_date: Date.strptime('08-21-2013', '%m-%d-%Y'),
 )
 Package.create(
   key: 'early-bird-all',
@@ -73,5 +73,46 @@ Package.create(
   count: 0,
   max: 1,
   start_date: Date.strptime('05-01-2013', '%m-%d-%Y'),
-  end_date: Date.strptime('08-15-2013', '%m-%d-%Y'),
+  end_date: Date.strptime('08-21-2013', '%m-%d-%Y'),
+)
+
+Package.create(
+  key: 'standalone',
+  name: 'F!rosh Kit Standalone Package',
+  description: 'A standard F!rosh Kit. Includes entrance to all events (like a parade!), a collection of cool things (like a hard hat!), and lots more! This kit has tons of awesome stuff!',
+  price: 120,
+  count: 0,
+  max: 1,
+  start_date: Date.strptime('08-22-2013', '%m-%d-%Y'),
+  end_date: Date.strptime('09-02-2013', '%m-%d-%Y'),
+)
+Package.create(
+  key: 'farm',
+  name: 'F!rosh Kit + Hart House Farm Package',
+  description: 'A standard F!rosh Kit just like above... except even better! This package includes a ticket to a camping trip at Hart House Farm, located outside of Caledon on the scenic Niagara Escarpment. Come join us for some fun camping on September 7 and 8 to cap off F!rosh Week!',
+  price: 150,
+  count: 0,
+  max: 1,
+  start_date: Date.strptime('08-22-2013', '%m-%d-%Y'),
+  end_date: Date.strptime('09-02-2013', '%m-%d-%Y'),
+)
+Package.create(
+  key: 'commuter',
+  name: 'F!rosh Kit + Commuter Program Package',
+  description: "A standard F!rosh Kit... with living accommodations! This offer includes room and board at a downtown hostel for four nights so you can fully enjoy F!rosh Week's days and nights.",
+  price: 205,
+  count: 0,
+  max: 1,
+  start_date: Date.strptime('08-22-2013', '%m-%d-%Y'),
+  end_date: Date.strptime('09-02-2013', '%m-%d-%Y'),
+)
+Package.create(
+  key: 'all',
+  name: 'F!rosh Kit + Hart House Farm + Commuter Program Package',
+  description: 'This is for the F!rosh that wants to have the ultimate F!rosh Week experience. You get a standard F!rosh kit, accommodations throughout the week, and a ticket to the weekend Hart House Farm camping trip. Super fun!',
+  price: 235,
+  count: 0,
+  max: 1,
+  start_date: Date.strptime('08-22-2013', '%m-%d-%Y'),
+  end_date: Date.strptime('09-02-2013', '%m-%d-%Y'),
 )
