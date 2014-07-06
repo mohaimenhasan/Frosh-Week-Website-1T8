d7217ce8fe149f6b381c3cd9d7a3aa6417c9a7df
Rustom Shareef <rustom.shareef@gmail.com>
2014-07-06 14:02:39 -0400

    update max packages numbers

--- app/models/package.rb
+++ app/models/package.rb
@@ -44,14 +44,14 @@ class Package < ActiveRecord::Base
 
   def available_early_bird?
     early_count = Package.select("sum(count)").where("key IN ('early-bird-standalone', 'early-bird-with-farm', 'early-bird-width-commuter', 'early-bird-all')").sum(:count)
-    return true if early_count < 1
+    return true if early_count < 150
     return false
   end
 
   def available_middle_bird?
     if !available_early_bird?
       middle_count = Package.select("sum(count)").where("key IN ('middle-bird-standalone', 'middle-bird-with-farm', 'middle-bird-width-commuter', 'middle-bird-all')").sum(:count)
-      return true if middle_count < 5
+      return true if middle_count < 500
     end
     return false
   end
