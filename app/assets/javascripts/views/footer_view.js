24a93ebd2160229f861c0a24a072e56c1087b3fb
Jason <jasonsk.ng@mail.utoronto.ca>
2015-05-31 14:03:37 -0400

    DEBUG: snapchat

--- app/assets/javascripts/views/footer_view.js
+++ app/assets/javascripts/views/footer_view.js
@@ -1,15 +1,6 @@
 //General Index: snapchat message appear
 App.FooterView = Ember.View.extend({
   didInsertElement: function() {
-    this._super();
     Ember.Logger.log("toggled_footer");
-    Ember.run.next(function() {
-      this.$("#snapchat-footer-icon").click(function(e){
-        Ember.Logger.log("toggled_footer_run");
-        $("#snapchat-message").fadeIn("slow");
-        window.location.hash = "#snapchat-message";
-        e.stopImmediatePropagation();
-      };
-    };
   }
 });
