68ec222922df8f73d3e22b951153fef025964036
Jason <jasonsk.ng@mail.utoronto.ca>
2015-06-06 19:31:31 -0400

    DEBUG: registration

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -25,7 +25,7 @@ App.RouteList = [
       Ember.Object.create({ route: 'receipt', path: '/receipt' }),
       Ember.Object.create({ route: 'bursary', path: '/bursary' }),
       Ember.Object.create({ route: 'confirm', path: '/confirm/:user/:token'})
-      Ember.Object.create({ route: 'payment', path: '/payment' }),
+//      Ember.Object.create({ route: 'payment', path: '/payment' }),
     ]
   }),
   Ember.Object.create({
