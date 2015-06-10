239176ec6e344b15895277dcde7996be4d1286eb
Jason Ng <jasonsk.ng@mail.utoronto.ca>
2015-06-10 02:12:39 -0400

    Updated setup files and database

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 ruby '1.9.3'
 
 gem 'rails',                '~> 3.0'
+gem 'therubyracer'
 gem 'bcrypt-ruby'
 gem 'jquery-rails'
 gem 'ember-rails'
--- app/assets/javascripts/views/footer_view.js
+++ app/assets/javascripts/views/footer_view.js
@@ -1,12 +1,18 @@
 App.FooterView = Ember.View.extend({
   didInsertElement: function(){
     this.$("#snapchat-footer-icon").click(function(e){
-      Ember.Logger.log("toggled");
+
       $("#snapchat-footer-message").fadeToggle("slow");
       e.stopImmediatePropagation();
     });
+    //On document ready: preload-image
+    this.$(document).ready(function(){
+      var snap_grey = new Image()
+      snap_grey.src = "snapchat-gray.png"
+    });
+    //On document click: message should disappear
     this.$(document).click(function(e){
-      Ember.Logger.log("document");
+      
       if($("#snapchat-footer-icon").is(":visible") && 
         !$("#snapchat-footer-message").is(e.target)){
         $("#snapchat-footer-message").fadeOut("slow");
