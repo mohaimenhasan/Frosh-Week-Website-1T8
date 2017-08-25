fd6fdfdfe7f27e5032777cba1753843761ce6b0a
hu_wi <huwilly.sh@gmail.com>
2017-08-24 21:58:56 -0400

    Added sponsorship logo: BIRD and VRPlaying

---
+++ app/assets/javascripts/helpers/leedurhhf_helpers (DESKTOP-ER29HJ0's conflicted copy 2017-08-21).js
@@ -0,0 +1,62 @@
+
+Ember.Handlebars.registerHelper('ifBeforeDeadline', function(key, options) {
+  var leedurEnd = new Date('July 22 2017 23:59:59 EDT');
+  var fweekEnd = new Date('Aug 18 2016 23:59:59 EDT');
+  var shirtEnd = new Date('Oct 26 2017 23:59:59 EDT');
+  var current = new Date();
+  var _key = Em.get(this, key);
+  if(current < shirtEnd && _key == "leedur shirt"){
+    return options.fn(this);
+  }
+  else if(current < leedurEnd && _key == "leedur"){
+    return options.fn(this);
+  }
+  else if(current < leedurEnd && _key == "leedurnobus"){
+      return options.fn(this);
+  }
+  else if(current < fweekEnd && _key == "fweek"){
+    return options.fn(this);
+  }
+  else if(current < shirtEnd && _key == "shirt"){
+    return options.fn(this);
+  }
+
+  else {
+    return options.inverse(this);
+  }
+});
+
+
+Ember.Handlebars.registerHelper('ifSoldOut', function(left, options) {
+
+  var _left = Em.get(this, left);
+  if(_left <= 0) {
+      return options.fn(this);
+  }
+  else {
+      return options.inverse(this);
+  }
+});
+
+Ember.Handlebars.registerHelper('ifLeedur', function(key, left, options) {
+
+  var _left = Em.get(this, left);
+  var _key = Em.get(this, key);
+  if(_key === 'leedur' && _left > 0) {
+      return options.fn(this);
+  }
+  else {
+      return options.inverse(this);
+  }
+});
+
+Ember.Handlebars.registerHelper('ifLeedurNoBus', function(key, left, options){
+    var _left = Em.get(this, left);
+    var _key = Em.get(this, key);
+    if(_key === 'leedurnobus' && _left > 0){
+        return options.fn(this);
+    }
+    else {
+        return options.inverse(this);
+    }
+})
