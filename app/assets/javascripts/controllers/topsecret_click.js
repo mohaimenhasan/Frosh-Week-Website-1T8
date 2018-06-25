27bdc6d664ff8b9efda8d5e19593eb496af96364
Tech <technology@orientation.skule.ca>
2018-06-24 20:26:54 -0400

    button attempt 1 for enter

---
+++ app/assets/javascripts/controllers/topsecret_click.js
@@ -0,0 +1,64 @@
+function checkCode(){
+    var code;
+    code = document.getElementById('code').value;
+
+    // add all the secret codes below
+    if(code == 'ABCDEF'){
+      window.location.href = "https://www.youtube.com";
+    }
+      //ALPHA
+    if(code == 'GODIVAGODIVAGODIVA'){
+      window.location.href = "https://youtu.be/MyPRIuTDy3M";
+      //BETA
+    }else if (code == 'ULTIMATE_FROSH') {
+      window.location.href = "https://youtu.be/9xYPwxCLRzI";
+      //CHI
+    }else if (code == 'TOIKE_OIKE') {
+      window.location.href = "https://youtu.be/fDEsC68-C0I";
+      //DELTA
+    }else if (code == 'SOLAR_CAR') {
+      window.location.href = "https://youtu.be/q8TIqKWq05E";
+      //GAMMA
+    }else if (code == 'ENGSOC') {
+      window.location.href = "https://youtu.be/oV1ybPqpd6A";
+      //KAPPA
+    }else if (code == 'SATELLITE_DESIGN') {
+      window.location.href = "https://youtu.be/1XEgegdNaIk";
+      //LAMBDA
+    }else if (code == 'FROSH_HANDBOOK') {
+      window.location.href = "https://youtu.be/0qs0rxVIhcM";
+      //NI
+    }else if (code == 'RUREADYTODYE') {
+      window.location.href = "https://youtu.be/fs44rh-6QYo";
+      //OMEGA
+    }else if (code == 'S_U_D_S') {
+      window.location.href = "https://youtu.be/LVXMeVUt860";
+      //OMICRON
+    }else if (code == 'EAA_SPORTS') {
+      window.location.href = "https://youtu.be/0hEdnVl4EIw";
+      //PHI
+    }else if (code == 'HARDHAT<3') {
+      window.location.href = "https://youtu.be/bwJF_GpX3dg";
+      //PI
+    }else if (code == 'MR_B&G') {
+      window.location.href = "https://youtu.be/d_E88npNSXI";
+      //PSI
+    }else if (code == 'SKULE_CANNON_TM') {
+      window.location.href = "https://youtu.be/ZnMFUXoqLH0";
+      //RHO
+    }else if (code == 'IRON_RING') {
+      window.location.href = "https://youtu.be/7mNGVdKFgAs";
+      //SIGMA
+    }else if (code == 'LADY_GODIVA_MEMORIAL_BNAD') {
+      window.location.href = "https://youtu.be/X8U6Bau9oEE";
+      //TAU
+    }else if (code == 'S_K_U_L_E') {
+      window.location.href = "https://youtu.be/pwFLCchxCws";
+      //THETA
+    }else if (code == 'ROCK_OF_AJAX') {
+      window.location.href = "https://youtu.be/koOM0Hihn2k";
+      //ZETA
+    }else if (code == 'HARDHATCAFE') {
+      window.location.href = "https://youtu.be/606aSaZC7Bo";
+    }
+}
