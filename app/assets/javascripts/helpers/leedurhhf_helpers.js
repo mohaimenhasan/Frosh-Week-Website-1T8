593c1947c45acb69a3d9ee7bbda31c7f3d2a0461
Mohaimen Khan <mkhan@bluecatnetworks.com>
2018-07-10 20:45:24 -0400

    Trying to refactor leedurs site

--- app/assets/javascripts/helpers/leedurhhf_helpers.js
+++ app/assets/javascripts/helpers/leedurhhf_helpers.js
@@ -42,7 +42,7 @@ Ember.Handlebars.registerHelper('ifLeedur', function(key, left, options) {
 
   var _left = Em.get(this, left);
   var _key = Em.get(this, key);
-  if(_key === 'leedur' && _left > 0) {
+  if(_key == 'leedur' && _left > 0) {
       return options.fn(this);
   }
   else {
@@ -53,7 +53,7 @@ Ember.Handlebars.registerHelper('ifLeedur', function(key, left, options) {
 Ember.Handlebars.registerHelper('ifLeedurNoBus', function(key, left, options){
     var _left = Em.get(this, left);
     var _key = Em.get(this, key);
-    if(_key === 'fweek' && _left > 0){
+    if((_key == 'fweek' || _key == 'leedur' ) && _left > 0){
         return options.fn(this);
     }
     else {
